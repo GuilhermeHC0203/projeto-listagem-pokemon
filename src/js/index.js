daf66c51ab0411817cf8f5f7ef8f0373608d03f8
GuilhermeHC0203 <133544175+GuilhermeHC0203@users.noreply.github.com>
2023-05-13 22:37:49 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,9 @@
-/* comentar varias linhas  () [] */ 
-
-// para comentar uma unica linha 
-
-
-const botaoAlterarTema = document.getElementById("botao-alterar-tema"); 
-
-/* console.log mostrar codigo do js no navegador 
-getelementbyid mostrar o elemento id, dentro dos parenteses colocamos a id */
-
-/* como pegar o body sem o id abaixo */ 
-
-const body = document.querySelector("body");
-const imagemBotaoTrocaDeTema = document.querySelector(".image-botao")
-
-/* identificar o clique do mouse no botao tema */ 
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");  /* console.log mostrar codigo do js no navegador ---- getelementbyid mostrar o elemento id, dentro dos parenteses colocamos a id */
+const body = document.querySelector("body"); /* como pegar o body sem o id */ 
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao"); /* identificar o clique do mouse no botao tema */ 
 
 botaoAlterarTema.addEventListener("click", () => {
-    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
 
     body.classList.toggle("modo-escuro");
 
@@ -26,4 +13,3 @@ botaoAlterarTema.addEventListener("click", () => {
         imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
     }
 });
-
